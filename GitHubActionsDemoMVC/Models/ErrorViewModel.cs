75d41fa9545194b808332c765d007f2ce4fa6b10
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-08-22 22:24:54 +0530

    Add project files.

---
+++ GitHubActionsDemoMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitHubActionsDemoMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
